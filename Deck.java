4c79ef03ba7013ce8e794165774f7cdff4ee71a2
zeke-sys <zek3.isaac@gmail.com>
2025-02-13 15:01:13 -0700

    Methods were added and more scenarios solved

--- Deck.java
+++ Deck.java
@@ -1,7 +1,10 @@
 package week05Lab;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Deck { //Deck class to have list of card field called cards
 	
@@ -21,14 +24,57 @@ public class Deck { //Deck class to have list of card field called cards
 			for (String suit : suits) {
 					cards.add(new Card(names[i], suit, values[i]));
 			}
-		}
+		} //end of for loop
 	} //end of constructor
+	
+	
+	//Deck Shuffle Method
+	//The Collections class consists exclusively of static methods that operate on or return collections (Java provided).
+	public void shuffle() {
+		Collections.shuffle(cards); //Randomly permutes the specified list using a default source of randomness
+	}
+	
+	//Draw card from Deck
+	
+	public Card drawCard() {
+		return cards.isEmpty() ? null : cards.remove(0); //if the list contains elements, remove the first card
+	}
+	
+	//Method that takes an int as a parameter (representing the number of players for a game)
+	//and returns a Map<String, List<Card>> that represents each player
+	//(i.e., "Player 1", "Player 2", etc.) and their cards).
+	//This method creates a new instance of Deck, shuffle it, and deal the cards out to the "players" in the Map
+	
+	public Map<String, List<Card>> dealCards(int numPlayers) {
+		if (numPlayers < 1) {
+			throw new IllegalArgumentException("Number of players must be at least 1."); //making sure the right input is entered
+		}
+		
+		shuffle(); //shuffling before dealing
+		Map<String, List<Card>> playerHands = new HashMap<>();
+		
+		//initialize player hands
+		for(int i=1; i<=numPlayers; i++) {
+			playerHands.put("Player " + i, new ArrayList<>());
+		}
+		
+		int playerIndex = 1;
+		
+		while (!cards.isEmpty()) {
+			String player = "Player " + playerIndex;
+			playerHands.get(player).add(drawCard()); //Deal a card to current player
+			playerIndex = (playerIndex % numPlayers) + 1; //Move to the next player
+		}
+		
+		return playerHands;
+		
+	} //end of method
 	
 	
 	//Describe method
 	//In this describe method, the override annotation toSpring() was added to the Card class so
 	//we wouldn't have to use card.getName() and card.getSuit(), etc. 
-	//Cleaner code
+	//Cleaner and more reusable code
 	public void describe() {
 		System.out.println("\nThis deck has 52 cards, as follows:\n");
 		for (Card card: cards) {
--- Week05Lab.java
+++ Week05Lab.java
@@ -1,5 +1,8 @@
 package week05Lab;
 
+import java.util.List;
+import java.util.Map;
+
 public class Week05Lab {
 
 	public static void main(String[] args) {
@@ -17,6 +20,17 @@ public class Week05Lab {
 		 * Three will have a value of 3, ... and an Ace will have a value of 14.
 		 */
 		
+		// 1. Card Class:
+		// Create a class called Card to represent a standard playing card.
+		// Fields: The Card class should have the following fields:
+		// a. name field
+		// b. suit field
+		// c. value field for comparing against other cards.
+		//
+		// Methods: This class can have any useful method.
+		// a. describe() to display the card information to the Console.
+		// b. Getters & Setters
+		
 		System.out.println("\n- Question 1: Card Class\n");
 		// Add your code here to instantiate a Card
 		
@@ -27,7 +41,19 @@ public class Week05Lab {
 		// Call the describe method on the newly instantiated card.
 		
 		card.describe();
-		
+
+		// 2. Deck Class:
+		// Create a class called Deck.
+		// Fields: This class should have a list of Card field called cards
+		// that will hold all the cards in the deck.
+		// List<Card> cards = new ArrayList<Card>();
+		//
+		// Constructor: The constructor for the Deck Class should
+		// instantiate all 52 standard playing cards and add them to the cards list.
+		//
+		// Methods:
+		// a. describe() to describe the deck to the Console --
+		// print out all of the cards in the deck.
 		
 		System.out.println("\n- Question 2: Deck Class");
 	    // Add your code here to instantiate a Deck
@@ -43,14 +69,65 @@ public class Week05Lab {
 		System.out.println("\nQuestion 3: Deck shuffle() method");
 				
 		// Test your method here
+		
+		deck.shuffle();
 				
-				
-				
+						
 		// Call the describe method on the newly shuffled deck.
+		
+		System.out.println("\nThe newly shuffled deck: ");
+		deck.describe();
+		
+		
+		// 4. Deck draw() Method:
+		// Add a draw method within the Deck Class
+		System.out.println("\nQuestion 4: Deck draw() method");
+		
+		// Test your method here
+		
+		System.out.println("\nDrawing one card of the top: ");
+		
+		System.out.println(deck.drawCard());
+		
+		
+		// 5. Create Game Board:
+		// Create and test a method that takes an int as a parameter (representing the
+		// number of players for a game) and returns a Map<String, List<Card>>
+		// that represents each player (i.e., "Player 1", "Player 2", etc..)
+		// and their cards.
+		//
+		// The method should create a new instance of Deck, shuffle it,
+		// and deal the cards out to the "players" in the Map.
+		
+		System.out.println("\nQuestion 5: Create Game\n");
+		
+		// Call your method here
+		
+		Deck deck1 = new Deck();
+		
+		Map<String, List<Card>> hands = deck1.dealCards(4); //deal to 4 players
+		
+		//printing each player's hand
+		for (Map.Entry<String, List<Card>> entry : hands.entrySet()) {
+			System.out.println(entry.getKey() + "'s hand:");
+			for (Card card1: entry.getValue()) {
+				System.out.println(" " + card1);
+			}
+			
+			System.out.println();
+			
+		} //end of loop
+		
+		
+		
+		
+		
 
 		
 		
 		
+				
+				
 		
 		
 		
